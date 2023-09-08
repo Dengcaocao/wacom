305265f1243d1f3ab4b64346eec2a5408a1a854d
dw <1536698087@qq.com>
2023-09-08 17:26:38 +0800

    将pixi实例添加到页面

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <main>
-    <Tabbar />
-    <Side />
-  </main>
+  <section>
+    <section>
+      <Tabbar />
+      <Side />
+    </section>
+    <main ref="container"></main>
+  </section>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import * as PIXI from 'pixi.js'
 import Tabbar from '@/components/Tabbar.vue'
 import Side from '@/components/Side.vue'
+const container = ref<HTMLElement>()
+
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerWidth,
+  antialias: true,
+  resizeTo: window
+})
+
+onMounted(() => {
+  container.value?.appendChild(app.view as any)
+})
 </script>
