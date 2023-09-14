58f3b649f6123442eb23ea722e8122d2f387f609
dw <1536698087@qq.com>
2023-09-14 15:28:54 +0800

    为store添加类型

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from '@/stores'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,9 +1,25 @@
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useConfigStore = defineStore('config', () => {
-  const drawType = ref('select')
-  const updateDrawType = (type: string) => drawType.value = type
+type drawType = 'select' | 'rect' | 'diamond' | 'arc' | 'arrow' | 'line' | 'pen' | 'font' | 'pic'
 
-  return { drawType, updateDrawType }
+interface IContext {
+  width: number,
+  strokeColor: string,
+  fillColor: string,
+  alpha: 1
+}
+
+export const useConfigStore = defineStore('config', () => {
+  const drawType = ref<drawType>('select')
+  const updateDrawType = (type: drawType) => drawType.value = type
+
+  const context = reactive({
+    width: 1,
+    strokeColor: 'red',
+    fillColor: 'transparent',
+    alpha: 1
+  })
+
+  return { drawType, updateDrawType, context }
 })
