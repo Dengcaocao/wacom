02f674e0657a1faed98d716bfb0727a230bba8e4
dw <1536698087@qq.com>
2023-09-15 17:12:59 +0800

    添加透明背景

--- src/components/InputColor.vue
+++ src/components/InputColor.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid grid-flow-col auto-cols-min gap-1">
-    <input type="color" v-model="value" class="w-8 h-8" />
+    <input type="color" v-model="value" class="w-8 h-8" :class="value === 'transparent' && value" />
     <div class="input-container">
       <div class="shrink-0 w-8 h-full leading-8 text-center bg-theme-color-deep">#</div>
       <input v-model="showValue" type="text" class="w-full outline-0 flex-1 indent-2">
@@ -23,7 +23,11 @@ const value = computed({
     emit('update:modelValue', value)
   }
 })
-const showValue = computed(() => props.modelValue.slice(-6))
+const showValue = computed(() => {
+  return /^#/.test(props.modelValue)
+    ? props.modelValue.slice(-6)
+    : props.modelValue
+})
 </script>
 
 <style scoped>
@@ -36,5 +40,12 @@ const showValue = computed(() => props.modelValue.slice(-6))
     border-color: theme('colors.theme-color-deep');
     @apply flex w-40 h-8 rounded overflow-hidden
   }
+}
+.transparent {
+  background-image: url('@/assets/bg-transparent.png');
+  background-size: cover;
+}
+.transparent::-webkit-color-swatch {
+  width: 0;
 }
 </style>
