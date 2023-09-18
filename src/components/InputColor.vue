5df21e34c43902b18c6a1538847f356c27c3a77a
dw <1536698087@qq.com>
2023-09-18 15:41:31 +0800

    修复不能设置值的问题

--- src/components/InputColor.vue
+++ src/components/InputColor.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid grid-flow-col auto-cols-min gap-1">
-    <input type="color" v-model="colorValue" class="w-8 h-8" :class="value === 'transparent' && value" />
+    <input type="color" v-model="colorValue" class="w-8 h-8" :class="inputValue === 'transparent' && inputValue" />
     <div class="input-container">
       <div class="shrink-0 w-8 h-full leading-8 text-center bg-theme-color-deep">#</div>
-      <input v-model="value" type="text" class="w-full outline-0 flex-1 indent-2">
+      <input v-model="inputValue" type="text" class="w-full outline-0 flex-1 indent-2">
     </div>
   </div>
 </template>
@@ -15,18 +15,25 @@ const props = defineProps<{
   modelValue: string
 }>()
 const emit = defineEmits(['update:modelValue'])
-const value = computed({
+const colorValue = computed({
+  get () {
+    return props.modelValue
+  },
+  set (value) {
+    emit('update:modelValue', value)
+  }
+})
+const inputValue = computed({
   get () {
     return /^#/.test(props.modelValue)
-    ? props.modelValue.slice(1)
-    : props.modelValue
+      ? props.modelValue.slice(1)
+      : props.modelValue
   },
   set (value) {
     if (value.length < 6) return
     emit('update:modelValue', `#${value}`)
   }
 })
-const colorValue = computed(() => props.modelValue)
 </script>
 
 <style scoped>
