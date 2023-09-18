f7eb70a4ad23b57be4fe613e3bcbc732251181a9
dw <1536698087@qq.com>
2023-09-18 10:06:14 +0800

    修复字符串切割导致的问题

--- src/components/InputColor.vue
+++ src/components/InputColor.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid grid-flow-col auto-cols-min gap-1">
-    <input type="color" v-model="value" class="w-8 h-8" :class="value === 'transparent' && value" />
+    <input type="color" v-model="colorValue" class="w-8 h-8" :class="value === 'transparent' && value" />
     <div class="input-container">
       <div class="shrink-0 w-8 h-full leading-8 text-center bg-theme-color-deep">#</div>
-      <input v-model="showValue" type="text" class="w-full outline-0 flex-1 indent-2">
+      <input v-model="value" type="text" class="w-full outline-0 flex-1 indent-2">
     </div>
   </div>
 </template>
@@ -17,17 +17,16 @@ const props = defineProps<{
 const emit = defineEmits(['update:modelValue'])
 const value = computed({
   get () {
-    return props.modelValue
+    return /^#/.test(props.modelValue)
+    ? props.modelValue.slice(1)
+    : props.modelValue
   },
   set (value) {
-    emit('update:modelValue', value)
+    if (value.length < 6) return
+    emit('update:modelValue', `#${value}`)
   }
 })
-const showValue = computed(() => {
-  return /^#/.test(props.modelValue)
-    ? props.modelValue.slice(-6)
-    : props.modelValue
-})
+const colorValue = computed(() => props.modelValue)
 </script>
 
 <style scoped>
