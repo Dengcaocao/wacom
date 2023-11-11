6fca3189849be1f8fdec05ba0fc12651a2ee3494
Deng`·caoaco <dengcaocao.dcc@gamil.com>
2023-11-11 13:40:49 +0800

    矩形绘制风格

--- src/hooks/drawRect.ts
+++ src/hooks/drawRect.ts
@@ -1,20 +1,76 @@
 import * as PIXI from 'pixi.js'
 
-const computedScalePoint = (arr: number[]) => arr.map(item => item)
+interface IPoint {
+  x: number
+  y: number
+}
+
+const offset = () => Math.random() * 10 - 5
+
+/**
+ * 朴素风格
+ * @param graphics 图形对象 
+ * @param downPoint 鼠标按下点
+ * @param movePoint 鼠标移动点
+ */
+const simpleStyle = (graphics: PIXI.Graphics, downPoint: IPoint, movePoint: IPoint) => {
+  const { x, y } = downPoint
+  const { x: mx, y: my } = movePoint
+  graphics.beginFill(0xff0000, 1)
+  graphics.drawPolygon([
+    x, y,
+    mx, y,
+    mx, my,
+    x, my
+  ])
+  graphics.endFill()
+}
+
+/**
+ * 手绘风格
+ * @param graphics 
+ * @param downPoint 
+ * @param movePoint 
+ */
+const handDrawnStyle = (graphics: PIXI.Graphics, downPoint: IPoint, movePoint: IPoint) => {
+  const { x, y } = downPoint
+  const { x: mx, y: my } = movePoint
+  const width = mx - x
+  const height = my - y
+  graphics.lineStyle({
+    width: 1,
+    color: 0x000000,
+    alpha: 1
+  })
+  graphics.moveTo(x + offset(), y + offset())
+  graphics.quadraticCurveTo(x + width/2, y + offset(), mx, y)
+  graphics.moveTo(x + offset(), y + offset())
+  graphics.quadraticCurveTo(x + width/2, y + offset(), mx, y)
+
+  graphics.moveTo(mx + offset(), y + offset())
+  graphics.quadraticCurveTo(mx, y + height/2, mx + offset(), my + offset())
+  graphics.moveTo(mx + offset(), y + offset())
+  graphics.quadraticCurveTo(mx, y + height/2, mx + offset(), my + offset())
+
+  graphics.moveTo(mx + offset(), my + offset())
+  graphics.quadraticCurveTo(x + width/2, my + offset(), x, my)
+  graphics.moveTo(mx + offset(), my + offset())
+  graphics.quadraticCurveTo(x + width/2, my + offset(), x, my)
+
+  graphics.moveTo(x + offset(), my + offset())
+  graphics.quadraticCurveTo(x, y + height/2, x + offset(), y + offset())
+  graphics.moveTo(x + offset(), my + offset())
+  graphics.quadraticCurveTo(x, y + height/2, x + offset(), y + offset())
+}
 
 export const useDrawRect = (CreateSceen: any) => {
   CreateSceen.prototype.drawRect = function (mx: number, my: number) {
     this.ghContainer.removeChildren()
     const graphics = new PIXI.Graphics()
     this.ghContainer.addChild(graphics)
-    graphics.beginFill(0xff0000, 1)
-    const { x, y } = this.downPoint
-    graphics.drawPolygon(computedScalePoint([
-      x, y,
-      mx, y,
-      mx, my,
-      x, my
-    ]))
-    graphics.endFill()
+    // simpleStyle(graphics, this.downPoint, {x: mx, y: my})
+    handDrawnStyle(graphics, this.downPoint, {x: mx, y: my})
+    // this.ghContainer.x = x
+    // this.ghContainer.y = y
   }
 }
