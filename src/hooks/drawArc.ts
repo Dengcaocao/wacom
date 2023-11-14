d1162cf1910b6c447624ad454456b991d424dc1c
Deng`·caoaco <dengcaocao.dcc@gamil.com>
2023-11-14 22:50:23 +0800

    fix 修复反向绘制出现镂空的问题

--- src/hooks/drawArc.ts
+++ src/hooks/drawArc.ts
@@ -12,7 +12,7 @@ export const useDrawArc = (CreateSceen: any) => {
     this.ghContainer.addChild(graphics)
     // 获取图形在容器中位置，并设置随机偏移点
     const index = this.ghContainer.getChildIndex(graphics)
-    this.ghContainer.offsetPoints[index] = this.ghContainer.offsetPoints[index] || createOffsetArr(4, 3)
+    this.ghContainer.offsetPoints[index] = this.ghContainer.offsetPoints[index] || createOffsetArr(4, 2)
     this.setGraphicsStyle(graphics)
     const { x, y } = this.downPoint
     const width = mx - x, height = my - y
@@ -26,8 +26,8 @@ export const useDrawArc = (CreateSceen: any) => {
     graphics.drawEllipse(
       x + width / 2,
       y + height / 2,
-      width / 2,
-      height / 2
+      Math.abs(width / 2),
+      Math.abs(height / 2)
     )
     stroke(graphics, vertex, this.ghContainer.offsetPoints[index])
     this.fillBgColor(graphics, vertex)
