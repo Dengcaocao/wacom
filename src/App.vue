43be20db75fdbe1f46e4a727d67e730168307c1e
DW <dengcaocao.dcc@gamil.com>
2023-11-14 15:37:51 +0800

    feat 引入字体

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,10 @@
 <template>
   <RouterView />
 </template>
+
+<style scoped>
+@font-face {
+  font-family: "LongCang-Regular";
+  src: url("@/assets/font/LongCang-Regular.ttf") format("truetype");
+}
+</style>
