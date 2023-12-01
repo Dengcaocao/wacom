71cb3f274d5ac08827f503d2995bf0fb589db443
DW <dengcaocao.dcc@gamil.com>
2023-12-01 10:33:09 +0800

    feat 弹窗组件

---
+++ src/components/Pop.vue
@@ -0,0 +1,54 @@
+<template>
+  <Teleport to="body">
+    <div v-show="visible" class="modal">
+      <Transition name="slide-fade">
+        <div class="modal-content" v-show="visible">
+          <h3 class="title">
+            {{ props.title }}
+            <i class="iconfont icon-colse close" @click="visible = false"></i>
+          </h3>
+          <slot></slot>
+        </div>
+      </Transition>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, defineExpose } from 'vue'
+
+const props = defineProps<{
+  title: string
+}>()
+const visible = ref(false)
+defineExpose({
+  visible
+})
+</script>
+
+<style scoped>
+  .slide-fade-enter-active,
+  .slide-fade-leave-active {
+    @apply transition-all ease-out;
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateY(-100px);
+  }
+  .modal {
+    background-color: rgba(0, 0, 0, 0.6);
+    @apply absolute inset-x-0 inset-y-0 m-auto z-20
+           flex items-center justify-center
+           w-full h-full;
+  }
+  .modal-content {
+    @apply w-5/12 p-2.5 rounded bg-white opacity-100;
+  }
+  .title {
+    @apply relative mb-2.5 text-center text-lg font-medium;
+  }
+  .title .close {
+    @apply absolute right-0 text-lg text-slate-600 hover:text-slate-800 cursor-pointer;
+  }
+</style>
