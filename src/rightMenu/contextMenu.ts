26db595f86a9838a48a1d625f30733e2e442c300
DW <dengcaocao.dcc@gamil.com>
2024-01-08 14:59:09 +0800

    feat 新增菜单选项

--- src/rightMenu/contextMenu.ts
+++ src/rightMenu/contextMenu.ts
@@ -4,29 +4,42 @@ import { useConfigStore } from '@/stores/config'
 import { toRaw } from 'vue'
 
 interface IMenu {
+  icon?: string
   title: string
   value: string
-  status: boolean
+  status?: boolean // 图标显示与隐藏
 }
 
 const config = useConfigStore(pinia)
 
 const menuList: IMenu[] = [
   {
-    title: '隐藏/显示网格',
+    icon: 'icon-tuya_huabanfuben',
+    title: '显示网格',
     value: 'mesh',
     status: true
+  },
+  {
+    title: '置于底层',
+    value: 'bottom'
   }
 ]
 
-const getInnerText = () => {
-  return menuList.reduce((preContent, menu) => {
-    return preContent += `
-      <div class="menu-item" data-value="${menu.value}">
-        <i class="iconfont ${menu.status ? 'icon-tuya_huabanfuben' : 'ml-5'}"></i>
-        ${menu.title}
-      </div>
-    `
+// 延迟函数
+const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time))
+
+const getInnerText = async () => {
+  await delay(10)
+  const container = config.pixiApp.container
+  return menuList
+    .filter(item => !!container || item.value !== 'bottom')
+    .reduce((preContent, menu) => {
+      return preContent += `
+        <div class="menu-item" data-value="${menu.value}">
+          <i class="iconfont ${menu.status && menu.icon ? menu.icon : 'ml-5'}"></i>
+          ${menu.title}
+        </div>
+      `
   }, '')
 }
 
@@ -35,29 +48,37 @@ const handleCloseMenu = () => {
   if (menuContainer) menuContainer.parentNode?.removeChild(menuContainer)
 }
 
-const handleMenuClick = (e: Event) => {
+const handleMenuClick = async (e: Event) => {
   const target = <HTMLElement>e.target
   if (target.className !== 'menu-item') return
+  handleCloseMenu()
   const { value } = target.dataset
   const menu = menuList.find(menu => menu.value === value) as IMenu
   menu.status = !menu.status
   const parentNode = <HTMLElement>target.parentNode
-  parentNode.innerHTML = getInnerText()
+  parentNode.innerHTML = await getInnerText()
   const pixiApp = toRaw(config.pixiApp)
-  const meshElm = pixiApp.app.stage.getChildByName('mesh')
-  pixiApp.createMesh(!meshElm)
-  handleCloseMenu()
+  switch (value) {
+    case 'mesh': {
+      const meshElm = pixiApp.app.stage.getChildByName(value)
+      return pixiApp.createMesh(!meshElm)
+    }
+    case 'bottom': {
+      const { app, container } = config.pixiApp
+      return container && app.stage.setChildIndex(container, 1)
+    }
+  }
 }
 
-const handContextmenu = (e: MouseEvent) => {
-  e.preventDefault()
+const handContextmenu = async (e: MouseEvent) => {
   handleCloseMenu()
   if ((<HTMLElement>e.target).tagName !== 'CANVAS') return
+  e.preventDefault()
   const menuContainer = document.createElement('div')
   menuContainer.setAttribute('class', 'context-menu-container')
   menuContainer.addEventListener('click', handleMenuClick)
   menuContainer.addEventListener('touchstart', handleMenuClick)
-  menuContainer.innerHTML = getInnerText()
+  menuContainer.innerHTML = await getInnerText()
   document.body.appendChild(menuContainer)
   const { width, height } = getComputedStyle(menuContainer)
   const x = window.innerWidth - e.x < parseInt(width) ? e.x - parseInt(width) : e.x
